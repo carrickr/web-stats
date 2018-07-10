bb685d8156cc55357dbc1db3fea5673117a4f40d
Carrick Rogers <carrickr@umich.edu>
2018-07-09 23:38:33 -0400

    add in hash generation for the site and tests

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,29 @@
-class Site < Sequel::Model
+# frozen_string_literal: true
 
+# Used for storing visits to websites
+# @attr [String] site the url of the website that was visited
+# @attr [String, Nil] referrer the referrer, if applicable, to the site visited
+# @attr [Time] created_at the time the site was visited, always in utc
+# @attr [String] hash a MD5 hash of all values above, excluding nil, values.
+# @since 0.0.1
+class Site < Sequel::Model
+  def after_save
+    generate_hash
+  end
+
+  def daily_count(day)
+    Site.where(created_at: day.utc.all_day).group(:url).size
+  end
+
+  def hash
+    self[:hash]
+  end
+
+  private
+
+  def generate_hash
+    t_hash = { id: id, url: url, referrer: referrer, created_at: created_at }
+    t_hash.delete(:referrer) if t_hash[:referrer].nil?
+    update(hash: Digest::MD5.hexdigest(t_hash.to_s))
+  end
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,42 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'the MD5 Digest' do
+    let!(:fixed_timestamp) { Time.now }
+    let!(:site_with_referrer) do
+      described_class.create(url: 'http://apple.com',
+                             referrer: 'http://developer.apple.com',
+                             created_at: fixed_timestamp)
+    end
+    let!(:site_nil_referrer) do
+      described_class.create(url: 'http://apple.com',
+                             referrer: nil,
+                             created_at: fixed_timestamp)
+    end
+
+    describe '.hash' do
+      it 'overrides the default hash method and returns a string' do
+        expect(site_with_referrer.hash.class).to eq(String)
+      end
+    end
+
+    describe '.generate_hash' do
+      it 'includes a non-nil referrer when generating the hash' do
+        expect(site_with_referrer.hash).to eq(Digest::MD5.hexdigest({ id: site_with_referrer.id,
+                                                                      url: 'http://apple.com',
+                                                                      referrer: 'http://developer.apple.com',
+                                                                      created_at: fixed_timestamp }.to_s))
+      end
+
+      it 'does not include a nil referrer when generating the hash' do
+        expect(site_nil_referrer.hash).to eq(Digest::MD5.hexdigest({ id: site_nil_referrer.id,
+                                                                     url: 'http://apple.com',
+                                                                     created_at: fixed_timestamp }.to_s))
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
